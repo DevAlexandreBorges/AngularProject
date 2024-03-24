8506ced397fcd7fd17d236e1dc92c25ef0a656d9
GabrielGarcia10 <garcia.gabrielrodrigues1@gmail.com>
2024-03-24 17:17:51 -0300

    Estrutura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { MenuLateralComponent } from './components/menu-lateral/menu-lateral.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { NotasComponent } from './components/notas/notas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, RodapeComponent, MenuLateralComponent, NotasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
